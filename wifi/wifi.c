eeab6d93e5c91c431a3fdb8d04f5439b75061d26
jbnouveau2004 <jb.storti@gmail.com>
2026-05-23 20:03:53 +0200

    dhcp, mac et hostname

--- wifi/wifi.c
+++ wifi/wifi.c
@@ -8,7 +8,7 @@
 
 #include "wifi.h"
 
-#define WIFI_SSID "TP-Link_2B38_EXT"
+#define WIFI_SSID "TP-Link_2B38"
 #define WIFI_PASSWORD "20947170"
 
 int wifi_connect(void)
@@ -22,6 +22,20 @@ int wifi_connect(void)
 
     printf("Connexion WiFi...\n");
 
+    // pour une visibilité sur le routeur
+    netif_set_hostname(netif_default, "pico-server");
+
+    // imposer une adresse IP hors plage et réservation DHCP
+    ip4_addr_t ip, mask, gw;
+    IP4_ADDR(&ip,   192,168,1,20);
+    IP4_ADDR(&mask, 255,255,255,0);
+    IP4_ADDR(&gw,   192,168,1,1);
+    dhcp_stop(netif_default);
+    netif_set_addr(netif_default,
+               &ip,
+               &mask,
+               &gw);
+
     if (cyw43_arch_wifi_connect_timeout_ms(
             WIFI_SSID,
             WIFI_PASSWORD,
@@ -33,6 +47,13 @@ int wifi_connect(void)
 
     printf("WiFi connecté\n");
     printf("IP: %s\n", ip4addr_ntoa(netif_ip4_addr(netif_default)));
+
+    // affiche l'adresse MAC
+    uint8_t mac[6];
+    cyw43_wifi_get_mac(&cyw43_state, CYW43_ITF_STA, mac);
+    printf("MAC: %02X:%02X:%02X:%02X:%02X:%02X\n",
+        mac[0], mac[1], mac[2],
+        mac[3], mac[4], mac[5]);
 
     return 0;
 }
